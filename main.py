f569c88b0153aeeb776cd0b1c80b316c7eb83a00
Laoliu-Glitch <15059512007@163.com>
2024-12-07 21:25:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-a = float(input("是否创建新的？Y/N"))
+frist = input("是否创建新的用户？Y/N:")
+i = 0
+users = {}
+if frist == "Y":
+    user_name = input("请输入用户名:")
+    while True:
+        i += 1
+        users['user_name',i] = user_name
+        print(users)
+        break
+elif frist == "N":
+    print("再见。")
+else:
+    print("请输入Y或者N。")
+    
