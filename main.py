8385dff1a248ab9ec89ddf4fbfa8285551b09ee1
Laoliu-Glitch <15059512007@163.com>
2024-12-07 21:44:46 +0800

    架构调整1.0.1

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-def else_sys():
-    frist = input("是否创建新的用户？Y/N:")
-    i = 0
-    users = {}
-    if frist == "Y":
-        user_name = input("请输入用户名:")
-        user_code = input("请输入密码：")
-        while True:
-            i += 1
-            users[user_name,i] = user_code
-            print(users)
-            break
-    elif frist == "N":
-        print("再见。")
-    else:
-        print("请输入Y或者N。")
+from else_sys import else_sys
 frist = input("是否创建新的用户？Y/N:")
 i = 0
 users = {}
@@ -30,4 +15,3 @@ elif frist == "N":
 else:
     print("请输入Y或者N。")
     else_sys()
-    
