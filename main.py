07ee36c5392d4f85c60bf73436a559bbf17f7a1d
Laoliu-Glitch <15059512007@163.com>
2024-12-07 21:28:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-frist = input("是否创建新的用户？Y/N:")
 i = 0
 users = {}
+def frst():
+    frist = input("是否创建新的用户？Y/N:")
 def user_sys():
     while True:
         i += 1
         users['user_name',i] = user_name
         print(users)
         break
+frst()
 if frist == "Y":
     user_name = input("请输入用户名:")
     user_sys()
@@ -14,3 +16,4 @@ elif frist == "N":
     print("再见。")
 else:
     print("请输入Y或者N。")
+    frst()
