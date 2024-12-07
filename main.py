30aa19ee0051a9ebd16adeedae63f217021be84c
Laoliu-Glitch <15059512007@163.com>
2024-12-07 21:38:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-frist = input("是否创建新的用户？Y/N:")
+def else_sys():
+    frist = input("是否创建新的用户？Y/N:")
 i = 0
 users = {}
 if frist == "Y":
     user_name = input("请输入用户名:")
+    user_code = input("请输入密码：")
     while True:
         i += 1
-        users['user_name',i] = user_name
+        users[user_name,i] = user_code
         print(users)
         break
 elif frist == "N":
     print("再见。")
 else:
     print("请输入Y或者N。")
+frist = input("是否创建新的用户？Y/N:")
+i = 0
+users = {}
+if frist == "Y":
+    user_name = input("请输入用户名:")
+    user_code = input("请输入密码：")
+    while True:
+        i += 1
+        users[user_name,i] = user_code
+        print(users)
+        break
+elif frist == "N":
+    print("再见。")
+else:
+    print("请输入Y或者N。")
+    else_sys()
